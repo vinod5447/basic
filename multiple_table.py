b8eeaad9eeaaef2c6cb807a2770a5f3457619c6e
vinod5447 <52646758+vinod5447@users.noreply.github.com>
2019-07-08 19:49:11 +0530

    Create multiple_table.py

---
+++ multiple_table.py
@@ -0,0 +1,23 @@
+# multiple
+#exp 5 * 1 = 5
+#    5 * 2 = 10
+#    5 * 3 = 15
+
+#exmple:1
+a =int(input("enter number:"))
+
+c=0
+for i in range(1,11):
+    c=c+1
+    z=a*c
+    
+    print(a,"*",c,"=",z)
+
+print("\n")
+print("\n")
+
+#exmple:2
+b =int(input("enter number:"))
+for j in range(1,11):
+    x=b*j
+    print(b,"*",j,"=",x)
